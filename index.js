0ad85b5d750e6c9fae912654c2f906095d74aa1c
Inda Smith <inda.k.smith@gmail.com>
2023-03-26 16:31:16 -0400

    no tests are passing, cannot figure out what is wrong.

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 12;
+let num4 = 3;
 
 // Write your code here
+function multiply() {
+const multiply = 
+     num1 * num2;
+return multiply
+}
+
+
+
+function random() {
+let random = Math.floor(Math.random()) * 10 + 1 > 0
+return random
+}
+
+
+
+function mod() {
+let mod = 
+    12 / 3;
+return mod
+}
+
+function max() {
+    Math.max(16, 17, 18, 19, 20);
+    return max
+}
+
+
